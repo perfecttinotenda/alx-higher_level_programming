2089db260d49ace8330f907d1bc3c9591c63045c
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-05-19 12:35:21 -0700

    All files

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""Tapa Task 11"""
+def multiply_list_map(my_list=[], number=0):
+    return list(map(lambda n: n * number, my_list))
---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""zviri kuita"""
+def update_dictionary(my_dict, key, value):
+    my_dict[key] = value
+    return my_dict
