760f830578fa6075ca2bfd29bb721b7adc357532
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-08-14 19:46:42 +0200

    task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/* learning how to pass arguments */
+if (process.argv.length === 2) {
+    console.log('No argument');
+  } else if (process.argv.length === 3) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
+  }
+  
