ae9e5109b8512ed0617cf6b61096acdcaacdf291
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-08-14 19:56:13 +0200

    Task 2 Intent Fixed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 /* learning how to pass arguments */
 if (process.argv.length === 2) {
-  console.log('No argument');
+    console.log('No argument');
 } else if (process.argv.length === 3) {
-  console.log('Argument found');
+    console.log('Argument found');
 } else {
-  console.log('Arguments found');
+    console.log('Arguments found');
 }
